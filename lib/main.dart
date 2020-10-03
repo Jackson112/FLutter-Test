99d4bb09601f97b26987a10012de1e8248b58fd5
Jackson Lazaro <jacksonlayaroict@gmail.com>
2020-10-03 06:38:48 -0700

    AddingThe Codes To Stateless Widget
    The stateless widget refreshes the changes automatically, without doing any reload after making changes to the codes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,45 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main() =>
   runApp(MaterialApp(
-    home: Scaffold(
-     appBar: AppBar(
+      home: Home(),
 
-       title: Text('Hello Flutter'),
-       backgroundColor: Colors.red[600],
-       centerTitle: true,
-
-     ),
-
-      body: Center(
-        child: Text('Hello Flutter Test',
-        style: TextStyle(
-            fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.grey,
-        ),
-        ),
-      ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('click'),
-          backgroundColor: Colors.red[600],
-
-    ),
-
-    ),
   ));
-}
+
+  class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+  appBar: AppBar(
+
+  title: Text('Hello Flutter'),
+  backgroundColor: Colors.red[600],
+  centerTitle: true,
+
+  ),
+
+  body: Center(
+  child: Text('Hello Flutter Test',
+  style: TextStyle(
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+  color: Colors.grey,
+  ),
+  ),
+  ),
+  floatingActionButton: FloatingActionButton(
+  onPressed: () {},
+  child: Text('click'),
+  backgroundColor: Colors.red[600],
+
+  ),
+
+  );
+
+  }
+  }
+
+
+
 
 
